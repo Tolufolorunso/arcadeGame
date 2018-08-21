2b65d04da004940037a53cd5e6cbc7894fe7e9ec
Tolulope folorunso <tolufolorunso@yahoo.com>
2018-08-20 18:16:01 -0700

    Updated

--- js/app.js
+++ js/app.js
@@ -239,3 +239,70 @@ document.addEventListener('keyup', function (e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+document.addEventListener("touchstart", startTouch, false);
+document.addEventListener("touchmove", moveTouch, false);
+ 
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+ 
+function startTouch(e) {
+  initialX = e.touches[0].clientX;
+  initialY = e.touches[0].clientY;
+};
+ 
+function moveTouch(e) {
+  if (initialX === null) {
+    return;
+  }
+ 
+  if (initialY === null) {
+    return;
+  }
+ 
+  var currentX = e.touches[0].clientX;
+  var currentY = e.touches[0].clientY;
+ 
+  var diffX = initialX - currentX;
+  var diffY = initialY - currentY;
+ 
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    // sliding horizontally
+    if (diffX > 0 && player.x > 0) {
+    // swiped left
+    console.log("swiped left");
+    player.x -= 101;
+    } if (diffX < 0 && player.x < 601) {
+    // swiped right
+    console.log("swiped right");
+    player.x += 101;
+    }
+    } else {
+    // sliding vertically
+    if (diffY > 0 && player.y > 0) {
+    // swiped up
+    console.log("swiped up");
+    player.y -= 83;
+    
+    } if (diffY < 0 && player.y < 540) {
+    // swiped down
+    console.log("swiped down");
+    player.y += 83;
+    }
+    
+    if (player.y < 0) {
+    setTimeout(function () {
+    player.reset();
+    }, 200);
+    player.counter++;
+    level();
+    player.winner();
+    }
+  }
+ 
+  initialX = null;
+  initialY = null;
+   
+  e.preventDefault();
+};
